53b73f971ecf1e0bbc8214ae616bbf7851e2872d
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-02-17 15:29:00 +0530

    refactor: remove trigger source arn variable

--- modules/lambda/varaible.tf
+++ modules/lambda/varaible.tf
@@ -8,11 +8,6 @@ variable "iam_role_arn" {
   type        = string
 }
 
-variable "trigger_source_arn" {
-  description = "arn for s3 bucket trigger"
-  type        = string
-}
-
 variable "s3_bucket_name" {
   description = "name or arn in S3 bucket"
   type        = string
