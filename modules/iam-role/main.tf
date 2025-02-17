82f2ea1de9b81f85d885d117a5124db563039405
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-02-17 13:06:58 +0530

    fix: json encode error

--- modules/iam-role/main.tf
+++ modules/iam-role/main.tf
@@ -2,7 +2,7 @@
 resource "aws_iam_role" "lambda_role" {
   name = var.role_name
 
-  assume_role_policy = jsoncode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
