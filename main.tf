4eb276be430ee1800177d0ae9252de842afed601
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-02-17 15:30:30 +0530

    feat: call lambda function :ox:

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ module "iam-role" {
   source    = "./modules/iam-role"
   role_name = "lambda_rekognition_s3_sns_role"
 }
+
+module "lambda_function" {
+  source         = "./modules/lambda"
+  function_name  = "image-classification-function"
+  iam_role_arn   = module.iam-role.lambda_role_arn
+  s3_bucket_name = module.s3-bucket.bucket_name
+  depends_on     = [module.iam-role, module.s3-bucket]
+}
