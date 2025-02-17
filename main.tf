d527082485ce21d17e8fb3c8d9fd6cb02328d056
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-02-17 09:42:35 +0530

    feat: call iam role

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ module "s3-bucket" {
   source        = "./modules/s3-bucket"
   bucket_prefix = "resource"
 }
+
+module "iam-role" {
+  source    = "./modules/iam-role"
+  role_name = "lambda_rekognition_s3_sns_role"
+}
--- modules/iam-role/variables.tf
+++ modules/iam-role/variables.tf
@@ -1,5 +1,4 @@
 variable "role_name" {
   description = "name for lambda iam role"
   type        = string
-  default     = "lambda_rekognition_s3_sns_role"
 }
