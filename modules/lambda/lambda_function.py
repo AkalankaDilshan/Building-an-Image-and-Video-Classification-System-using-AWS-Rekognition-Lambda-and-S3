13b98c81968b266996a82fd02b1af28f8ab06f67
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-02-17 13:00:05 +0530

    feat: roughly complete lambda without sns

--- modules/lambda/lambda_function.py
+++ modules/lambda/lambda_function.py
@@ -20,17 +20,41 @@ def lambda_handler(event, context) :
      
      rekognition_client = boto3.client('rekognition')
      
-     response_rekognition = rekognition_client.detect_labels(
-          Image={
-               'S3Object' : {
-                    'Bucket' : bucket,
-                    'Name' : image
-               }
-          },
-          MinConfidence = 70
+     try:
+          response_rekognition = rekognition_client.detect_labels(
+               Image={
+                    'S3Object' : {
+                         'Bucket' : bucket,
+                         'Name' : image
+                    }
+               },
+               MinConfidence = 70
+          )
+     
+          detected_lables = []
+     
+          if response_rekognition['Labels']:
+               for label in response_rekognition['Labels']:
+                    detected_lables.append(label['Name'].lower())
+               print(detected_lables)
+          
+          
+               if LABEL in detected_lables:
+                    response['Status'] = f"Success.! {LABEL} found"
+                    response['body'].append(response_rekognition['Labels']) 
+               else:
+                    response['Status'] = f"Failled.! {LABEL} Not found"
+                    response['body'].append(response_rekognition['Labels']) 
+
+     except Exception as error:
+          print(error)          
+          
+     
+     s3_client.put_object(
+          Bucket = bucket,
+          Key = output_key,
+          Body = json.dumps(response, indent=4)
      )
      
-     detected_lables = []
-     
-     if response_rekognition['']
+     return response     
      
